db9d769d60f02782b53b215927a140194bcc9581
beerappabvgp <beerappabharathb@gmail.com>
2024-06-07 11:23:18 +0530

    Learnt selection sort

--- sorting/selection.py
+++ sorting/selection.py
@@ -1,12 +1,14 @@
 class Solution:
     def selection(self , arr):
         for i in range(len(arr)):
+            mini = i
             for j in range(i + 1, len(arr)):
-                if arr[i] > arr[j]:
-                    arr[i] , arr[j] = arr[j],arr[i]
+                if arr[mini] > arr[j]:
+                    mini = j
+            arr[mini] , arr[i] = arr[i],arr[mini]
         return arr
 
 
 s = Solution()
-print(s.selection([4,1,2,7,5,0,45,2,34,21,34,67,54,79,100]))
+print(s.selection([4,1,2,7,5,0,300,400,45,2,34,21,34,67,54,79,100 , 200]))
 
