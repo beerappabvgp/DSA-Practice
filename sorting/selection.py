3dd1aeff2bb0eb34eeabc99f2e85a8264c849e80
beerappabvgp <beerappabharathb@gmail.com>
2024-06-07 11:26:09 +0530

    updated selection sort code

--- sorting/selection.py
+++ sorting/selection.py
@@ -1,14 +1,14 @@
 class Solution:
     def selection(self , arr):
-        for i in range(len(arr)):
-            mini = i
-            for j in range(i + 1, len(arr)):
-                if arr[mini] > arr[j]:
-                    mini = j
-            arr[mini] , arr[i] = arr[i],arr[mini]
+        for i in range(len(arr)-1):
+            min_idx = i
+            for j in range(i+1 , len(arr)):
+                if arr[min_idx] > arr[j]:
+                    min_idx = j
+            arr[i] , arr[min_idx] = arr[min_idx] , arr[i]
         return arr
 
 
 s = Solution()
-print(s.selection([4,1,2,7,5,0,300,400,45,2,34,21,34,67,54,79,100 , 200]))
+print(s.selection([4,1,2,7,5,0,300,10000,400,45,2,34,21,34,67,54,79,100 , 200]))
 
